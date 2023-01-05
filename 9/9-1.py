89e73502d60afa142028c4ffe8826ad4dd7da572
KarlDagenais <115113444+KarlDagenais@users.noreply.github.com>
2023-01-05 15:35:57 -0500

    day 9 part 1 complete

--- 9/9-1.py
+++ 9/9-1.py
@@ -1,12 +1,12 @@
 # lire le fichier d'instructions
 data=[]
-with open ("9.txt", "r") as f:
+with open ("/Users/karldagenais/Documents/VisualStudioCode/AdventOfCode2022/9/9.txt", "r") as f:
     for line in f:
         data.append(line.strip())    
-        
-# identifier coordonnées de départ et les include dans un dictionnaire
-coord = {}
-coord["s"]=(0,0)
+coord = []
+H=(0,0)
+T=(0,0)
+coord.append(T)
 
 #Créer fonction de mouvement avec en argument les coordonnées de H et T et la prochaine instruction
 
@@ -14,11 +14,12 @@ def mouvement (H, T,  direction):
     """Calcule les nouvelles coordonnées en fonction de la direction et de la distance
 
     Args:
-        depart (tuple): Les coordonnées de départ x, y
+        H (tuple): Les coordonnées de départ x, y de la head
+        T (tuple): Les coordonnées de départ x, y de la tail
         instruction (string): Les instructions du mouvememnt en cours
 
     Returns:
-        coordonnées: tuple des coordonnées de la tail
+        coordonnées: tuple des coordonnées de la head et de la tail
     """
     xh, yh = H
     xt, yt = T
@@ -46,24 +47,31 @@ def mouvement (H, T,  direction):
             yt += 1
             xt = xh
     
-    # elif direction == "D": #si la direction est en bas
-    #     if yh == yt or yh == yt+1: #si la tête est en haut de la queue ou au même endroit
-    #         xh -= 1
-    #     
-            
-            
+    elif direction == "D": #si la direction est en bas
+        if yh == yt or yh == yt+1: #si la tête est en haut de la queue ou au même endroit
+            yh -= 1
+        elif yh == yt-1: #si la tête est en bas de la queue
+            yh -= 1
+            yt -= 1
+            xt = xh
+       
     return (xh,yh), (xt,yt)
 
 #appeler la fonction de mouvement pour chaque instruction
-# for current in data:
-#     direction = data[current][0]
-#     distance = int(data[current][1:])
-#     mouvement(H, T, current)
-    
-    
-#enregistrer la nouvelle position dans un dictionnaire, ajouter 1 à la valeur de la clé si la position existe déjà
+for current in range(len(data)):
+    direction, distance = data[current].split(" ")
+    for i in range(int(distance)):
+        H, T = mouvement(H, T, direction)
+        if not T in coord: #ajoute les coordonnées de la queue à la liste des coordonnées si non présente
+            coord.append(T) 
+
+
+#compter le nombre d'entrées dans la liste
+print(f"La première réponse est {len(coord)}")
+
+
+#partie 2
 
 
 
-#compter le nombre de clées uniques dans le dictionnaire
 
